be4e7e39f53c62649b62c027ee9c7a0a4f05f615
MarcosMello <Marcosvinicius-vm@outlook.com>
2021-11-16 22:37:44 -0300

    Added TicTacToe grid lines, knots and crosses functionality and detection on click on frame.

--- Client.py
+++ Client.py
@@ -9,6 +9,11 @@ playersList = ["Player1", "Player2", "Player3", "Player4", "Player5", "Player6",
 leader = playersList[0]; #TMP - It'll be received as leader 
 idListPlayers = [];
 respOp = False;
+turn = True;
+
+p = True;
+
+al = [True, True, True, True, True, True, True, True, True];
 
 class GUI:
     def __init__(self, root):
@@ -73,7 +78,122 @@ class GUI:
         sepFr2.pack_propagate(False);
 
         game = tk.Frame(gameWin, bg = "yellow", height = 347, width = 350);
-        game.grid(column = 0, columnspan = 3, row = 4, rowspan = 6); 
+        game.grid(column = 0, columnspan = 3, row = 4, rowspan = 6);
+        game.pack_propagate(False);
+
+        quad1 = tk.Frame(game, bg = "yellow", height = 115, width = 116);
+
+        quad1.grid(column = 0, row = 0);
+
+        quad1.pack_propagate(False);
+        lquad1 = tk.Label(quad1, textvariable = lquad1Txt, bg = "yellow");
+        lquad1.pack(expand = True);
+
+        quad1.bind("<Button-1>", lambda event: click(event, 1) if (turn and al[0]) else None);
+
+        hv1 = tk.Frame(game, bg = "black", height = 115, width = 1);
+        hv1.grid(column = 1, row = 0);
+
+        quad2 = tk.Frame(game, bg = "yellow", height = 115, width = 116);
+
+        quad2.grid(column = 2, row = 0);
+
+        quad2.pack_propagate(False);
+        lquad2 = tk.Label(quad2, textvariable = lquad2Txt, bg = "yellow");
+        lquad2.pack(expand = True);
+
+        quad2.bind("<Button-1>", lambda event: click(event, 2) if (turn and al[1]) else None);
+
+        hv2 = tk.Frame(game, bg = "black", height = 115, width = 1);
+        hv2.grid(column = 3, row = 0);
+
+        quad3 = tk.Frame(game, bg = "yellow", height = 115, width = 116);
+
+        quad3.grid(column = 4, row = 0);
+
+        quad3.pack_propagate(False);
+        lquad3 = tk.Label(quad3, textvariable = lquad3Txt, bg = "yellow");
+        lquad3.pack(expand = True);
+
+        quad3.bind("<Button-1>", lambda event: click(event, 3) if (turn and al[2]) else None);
+
+        hh1 = tk.Frame(game, bg = "black", height = 1, width = 350);
+        hh1.grid(column = 0, row = 1, columnspan = 5);
+
+        quad4 = tk.Frame(game, bg = "yellow", height = 115, width = 116);
+
+        quad4.grid(column = 0, row = 2);
+
+        quad4.pack_propagate(False);
+        lquad4 = tk.Label(quad4, textvariable = lquad4Txt, bg = "yellow");
+        lquad4.pack(expand = True);
+
+        quad4.bind("<Button-1>", lambda event: click(event, 4) if (turn and al[3]) else None);
+
+        hv3 = tk.Frame(game, bg = "black", height = 115, width = 1);
+        hv3.grid(column = 1, row = 2);
+
+        quad5 = tk.Frame(game, bg = "yellow", height = 115, width = 116);
+
+        quad5.grid(column = 2, row = 2);
+
+        quad5.pack_propagate(False);
+        lquad5 = tk.Label(quad5, textvariable = lquad5Txt, bg = "yellow");
+        lquad5.pack(expand = True);
+
+        quad5.bind("<Button-1>", lambda event: click(event, 5) if (turn and al[4]) else None);
+
+        hv4 = tk.Frame(game, bg = "black", height = 115, width = 1);
+        hv4.grid(column = 3, row = 2);
+
+        quad6 = tk.Frame(game, bg = "yellow", height = 115, width = 116);
+
+        quad6.grid(column = 4, row = 2);
+
+        quad6.pack_propagate(False);
+        lquad6 = tk.Label(quad6, textvariable = lquad6Txt, bg = "yellow");
+        lquad6.pack(expand = True);
+
+        quad6.bind("<Button-1>", lambda event: click(event, 6) if (turn and al[5]) else None);
+
+        hh2 = tk.Frame(game, bg = "black", height = 1, width = 350);
+        hh2.grid(column = 0, row = 3, columnspan = 5);
+
+        quad7 = tk.Frame(game, bg = "yellow", height = 115, width = 116);
+
+        quad7.grid(column = 0, row = 4);
+
+        quad7.pack_propagate(False);
+        lquad7 = tk.Label(quad7, textvariable = lquad7Txt, bg = "yellow");
+        lquad7.pack(expand = True);
+
+        quad7.bind("<Button-1>", lambda event: click(event, 7) if (turn and al[6]) else None);
+
+        hv5 = tk.Frame(game, bg = "black", height = 115, width = 1);
+        hv5.grid(column = 1, row = 4);
+
+        quad8 = tk.Frame(game, bg = "yellow", height = 115, width = 116);
+
+        quad8.grid(column = 2, row = 4);
+
+        quad8.pack_propagate(False);
+        lquad8 = tk.Label(quad8, textvariable = lquad8Txt, bg = "yellow");
+        lquad8.pack(expand = True);
+
+        quad8.bind("<Button-1>", lambda event: click(event, 8) if (turn and al[7]) else None);
+
+        hv6 = tk.Frame(game, bg = "black", height = 115, width = 1);
+        hv6.grid(column = 3, row = 4);
+
+        quad9 = tk.Frame(game, bg = "yellow", height = 115, width = 116);
+
+        quad9.grid(column = 4, row = 4);
+
+        quad9.pack_propagate(False);
+        lquad9 = tk.Label(quad9, textvariable = lquad9Txt, bg = "yellow");
+        lquad9.pack(expand = True);
+
+        quad9.bind("<Button-1>", lambda event: click(event, 9) if (turn and al[8]) else None);
 
         score = tk.Frame(gameWin, bg = "purple", height = 171, width = 150);
         score.grid(column = 4, row = 0, rowspan = 5);
@@ -154,6 +274,18 @@ class players:
                     idListPlayers.append(pID);
                     pID.pack(expand = True);
 
+def click(event, quad):
+    global p;
+
+    if p: 
+        lquads[quad-1].set("X");
+        p = not p;
+        al[quad-1] = False;
+    else:
+        lquads[quad-1].set("O");
+        p = not p;
+        al[quad-1] = False;
+
 def openChat():
     if not chatOpen:
         global chatWinOBJ;
@@ -193,6 +325,18 @@ symbolTxt = tk.StringVar(value = "X");
 turnTxt = tk.StringVar(value = "SEU TURNO!");
 
 #TMP
+
+lquad1Txt = tk.StringVar(value = "");
+lquad2Txt = tk.StringVar(value = "");
+lquad3Txt = tk.StringVar(value = "");
+lquad4Txt = tk.StringVar(value = "");
+lquad5Txt = tk.StringVar(value = "");
+lquad6Txt = tk.StringVar(value = "");
+lquad7Txt = tk.StringVar(value = "");
+lquad8Txt = tk.StringVar(value = "");
+lquad9Txt = tk.StringVar(value = "");
+
+lquads = [lquad1Txt, lquad2Txt, lquad3Txt, lquad4Txt, lquad5Txt, lquad6Txt, lquad7Txt, lquad8Txt, lquad9Txt];
 
 app = GUI(root);
 gameScrn = GUI.gamewin(app);
